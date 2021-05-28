64e58572248cc5d2f86c430896d92d489e9e6bdd
Enio N. de Souza <enionsouza@gmail.com>
2021-05-28 15:44:50 -0400

    Add validations for database

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :posts
+
+  validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..16 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author
+
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true
+  validates :author_id, presence: true
 end
